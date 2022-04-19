aad73f3811e8ab8cae5b9a6cfe1da692d56b6442
Mwai <machariamwai@gmail.com>
2022-04-19 08:59:07 +0300

    Pass test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function(){
-  document.getElementById("text").innerHTML = "This is really cool!"
+  document.getElementById("text").innerHTML =    "This is really cool!"
 });
 
 
